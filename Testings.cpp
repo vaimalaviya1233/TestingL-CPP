4deecbd29caaf56c631e45041437830bb14636f8
Vaibhav Malaviya <55515799+vaimalaviya1233@users.noreply.github.com>
2023-02-08 16:03:56 -0600

    Add project files.

---
+++ Testings.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << "Starting Program" << endl;
+    
+
+    cout << endl << "Ended Program" << endl;
+    return 0;
+}
+
+/*
+*   double m = 2342;
+*   cout << setprecision(3) << fixed << m; // prints 2342.000
+*/
+
+/*
+* int age = 22;
+* cout << boolalpha << (age >= 21);
+*/
+
+
+/*
+* int distance = 21, speed = 2;
+* double Value = (static_cast<double>(distance) / speed);
+* cout << static_cast<double>(distance) / speed;
+*/
+
+/*
+* float test;
+* cout << "Starting Program" << endl;
+* test = 2.0e38 * 1000; // should overflow test
+* cout << test << endl;
+* test = 2.0e-38 / 2.0e38; // should underflow test
+* cout << test << endl;
+* return 0;
+*/
+// 1. {//string num = "num";
+    //string cost = "cost";
+    //cout << num << endl << " " << endl << cost;
+//}
+
+/*
+* int fu = 66934;
+* sizeof(fu);
+* return 0; 
+*/
+
+/*
+*    long double apple;
+*    cout << "The size of an integer is " << sizeof(int) << endl;
+*    cout << "The size of an long integer is " << sizeof(long) << endl;
+*    cout << "The size of an apple is " << sizeof(apple) << endl;
+*/
